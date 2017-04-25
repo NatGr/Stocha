5fb6fea9baa4ba0d5fa6c063418631a90aab0a32
NatGr <nathan.2@live.be>
2017-04-25 11:29:41 +0200

    gère les sons avec des 0

--- getCoef.m
+++ getCoef.m
@@ -1,10 +1,11 @@
 % function exporting the mel coefficients out of a given sound
 function obs = getCoef(sound, numberCep)
 fs = 44100;
-band = [300, 2500]/(fs/2);
+band = [10, 5000]/(fs/2);
 [B, A] = butter(2,band);
-sound = filter(B, A, sound);
-obs = melfcc(sound(:,1),fs,'numcep', numberCep);
+sound = sound(:,1); % in case of stereo sound
+sound = filter(B, A, sound(sound ~= 0));
+obs = melfcc(sound,fs,'numcep', numberCep);
 
 % TODO: remove
 if numel(find(isnan(obs))) ~= 0

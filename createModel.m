f57f603034d757ade1ca95cca204a43eadfd6bf9
HyperSeb <sebastien.blondiau@me.com>
2017-04-26 22:48:25 +0200

    modified filter and initialisation

--- createModel.m
+++ createModel.m
@@ -1,5 +1,5 @@
 % function building a model for a given digit
-function model = createModel(sounds, numberStates, numberGaussPerState, numberCep)
+function [model, LL] = createModel(sounds, numberStates, numberGaussPerState, numberCep)
 
 trainingData = cell(1,numel(sounds));
 for i = 1:numel(sounds)
@@ -9,15 +9,17 @@ end
 %prior0 = normalise(rand(numberStates,1));
 prior0 = normalise(initMat(numberStates, 1,@(i,j) i==1));
 % transmat0 = mk_stochastic(triu(rand(numberStates,numberStates)));
-transmat0 = mk_stochastic(initMat(numberStates, numberStates,@(i,j) (i<=j) .* (0.01 ^ abs(i-j))));
+% transmat0 = mk_stochastic(initMat(numberStates, numberStates,@(i,j) (i~=1 || j <= 2) * (i<=j) * (0.05 ^ abs(i-j))) .* rand(numberStates,numberStates));
+
+transmat0 = mk_stochastic(initMat(numberStates, numberStates,@(i,j) (i==j) + (i==j-1) * 0.05) .* rand(numberStates,numberStates));
 
 [mu0, Sigma0] = mixgauss_init(numberStates*numberGaussPerState, cell2mat(trainingData), 'diag', 'kmeans');
 mu0 = reshape(mu0, [numberCep numberStates numberGaussPerState]);
 Sigma0 = reshape(Sigma0, [numberCep numberCep numberStates numberGaussPerState]);
-%mixmat0 = mk_stochastic(rand(numberStates,numberGaussPerState));
-mixmat0 = mk_stochastic(ones(numberStates,numberGaussPerState));
+mixmat0 = mk_stochastic(rand(numberStates,numberGaussPerState));
+%mixmat0 = mk_stochastic(ones(numberStates,numberGaussPerState)); % RANDOM AGAIN
 
-[LL, model.pi, model.A, model.mu, model.sigma, model.B] = mhmm_em(trainingData, prior0, transmat0, mu0, Sigma0, mixmat0, 'verbose', 0);
+[LL, model.pi, model.A, model.mu, model.sigma, model.B] = mhmm_em(trainingData, prior0, transmat0, mu0, Sigma0, mixmat0, 'verbose', 0, 'max_iter', 25);
 
 
 end
--- getCoef.m
+++ getCoef.m
@@ -1,10 +1,10 @@
 % function exporting the mel coefficients out of a given sound
 function obs = getCoef(sound, numberCep)
 fs = 44100;
-band = [300, 2500]/(fs/2);
+band = [100, 6000]/(fs/2);
 [B, A] = butter(2,band);
-sound = filter(B, A, sound);
-obs = melfcc(sound(:,1),fs,'numcep', numberCep);
+sound = filter(B, A, sound(:,1));
+obs = melfcc(sound,fs,'numcep', numberCep);
 
 % TODO: remove
 if numel(find(isnan(obs))) ~= 0

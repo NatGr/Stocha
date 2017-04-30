d7b840df1f522168ebf94114688d780c182f06f8
NatGr <nathan.2@live.be>
2017-04-30 14:41:25 +0200

    update paramètres + fichiers arnaud

--- createModel.m
+++ createModel.m
@@ -7,15 +7,17 @@ for i = 1:numel(sounds)
 end
 
 prior0 = normalise(rand(numberStates,1));
-% transmat0 = mk_stochastic(triu(rand(numberStates,numberStates)));
-transmat0 = mk_stochastic(initMat(numberStates, numberStates,@(i,j) (i<=j) .* (0.01 ^ abs(i-j))));
+transmat0 = mk_stochastic(triu(rand(numberStates,numberStates)));
+% prior0 = zeros(numberStates,1);
+% prior0(1) = 1;
+% transmat0 = mk_stochastic(initMat(numberStates, numberStates,@(i,j) (i==j) + (i==j-1) * 0.05) .* rand(numberStates,numberStates));
 
 [mu0, Sigma0] = mixgauss_init(numberStates*numberGaussPerState, cell2mat(trainingData), 'diag');
 mu0 = reshape(mu0, [numberCep numberStates numberGaussPerState]);
 Sigma0 = reshape(Sigma0, [numberCep numberCep numberStates numberGaussPerState]);
 mixmat0 = mk_stochastic(rand(numberStates,numberGaussPerState));
 
-[LL, model.pi, model.A, model.mu, model.sigma, model.B] = mhmm_em(trainingData, prior0, transmat0, mu0, Sigma0, mixmat0, 'verbose', 0);
+[~, model.pi, model.A, model.mu, model.sigma, model.B] = mhmm_em(trainingData, prior0, transmat0, mu0, Sigma0, mixmat0, 'verbose', 0, 'max_iter', 25);
 
 
 end
--- predict.m
+++ predict.m
@@ -11,7 +11,7 @@
 % output : array of int
 %       The predicted digits associated to an element of speechs.
 function [output] = predict(speechs)
-models = arrayfun( @(digit) createModel(soundsForDigit(digit),4,4,17), 0:9);
+models = arrayfun( @(digit) createModel(soundsForDigit(digit),8,6,17), 0:9);
 output = arrayfun( @(e) findBestModel(e, models), speechs, 'UniformOutput', false) - 1;
 % the -1 corrects the fact that indexes begin at 1 and the data begin at 0
 end
